9819529891688864ee57a5db0f09fde54114ea2d
koksyncho <hrokishmokibarbaroki@gmail.com>
2017-04-07 21:29:14 +0300

    Added two attacks to Rattata
    Rattata is still not very interesting, but atleast it has more options to make.

--- src/pokemon/Rattata.java
+++ src/pokemon/Rattata.java
@@ -25,6 +25,8 @@ public class Rattata extends Pokemon implements AttacksNormal{
 	{
 		System.out.println("[1]Hyper Fang");
 		System.out.println("[2]Tackle");
+		System.out.println("[3]Hyper Tooth");
+		System.out.println("[4]Headbutt");
 		System.out.println("Rattata is not very interesting, is it.");
 	}
 	
